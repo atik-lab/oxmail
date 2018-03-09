7e3198898071e1f2b91b4f1a61f42151a2dbb8b3
Atik <atik.mail@protonmail.com>
2018-03-09 21:36:04 +0200

    Creation of an email.

--- contracts/Oxmail.sol
+++ contracts/Oxmail.sol
@@ -1,12 +1,42 @@
 pragma solidity ^0.4.19;
 
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
-
 /**
  * @title Oxmail
- * @dev Let users interact and send messages in the blockchain.
+ * @dev This contract stores the necessary information for the oxmail project.
  */
-contract Oxmail {
-    mapping (uint => address) public zombieToOwner;
+contract Oxmail
+{
+    // the mail struct and the array of mails
+    struct Mail {
+        string body;
+        address from;
+    }
+    Mail[] public mail;
 
+    // a list of the mail receivers
+    mapping(address => uint) public addressToMail;
+    // a list of the mail from
+    mapping(address => uint) public addressFromMail;
+
+    event NewMail(uint mailId, address from, address to);
+
+    /**
+     * @dev creates a mail
+     */
+    function _createMail(string _body) private returns(uint) {
+        require(_body != '');
+        Mail _mail = Mail(_body, msg.sender);
+        uint _id = mail.push(_mail);
+        addressFromMail[msg.sender] = _id;
+        return _id;
+    }
+
+    /**
+     * @dev creates a mail and sends it
+     */
+    function sendMail(string _body, address _to) {
+        uint _id = _createMail(_body);
+        addressToMail[_to] = _id;
+        NewMail(_id, msg.sender, _to);
+    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var Storage = artifacts.require("./Storage.sol");
+var Oxmail = artifacts.require("./Oxmail.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Storage);
+  deployer.deploy(Oxmail);
 };
